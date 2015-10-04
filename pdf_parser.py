d0e3c27e0a4f008aa55ef90a811461b50cdaa062
Yos Riady <yosriady@gmail.com>
2015-10-04 11:29:50 +0800

    Write PDFParser component

---
+++ pdf_parser.py
@@ -0,0 +1,34 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+
+def convert_to_txt(pdf):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(pdf, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+    pdf.close()
+    device.close()
+    retstr.close()
+    return text
+
+# Usage
+
+# import os
+# import pdf_parser
+# pdfpath = os.path.expanduser("~/CViA/pdfs/sample1.pdf")
+# pdf = file(pdfpath, 'rb')
+# pdf_parser.convert_to_txt(pdf)
