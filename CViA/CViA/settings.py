867db9e43dc9e407c19ad2c7c72b263f95a48eea
Su Gi Chandran <sollercoaster@hotmail.com>
2015-10-09 15:12:42 +0800

    Setup database

--- CViA/CViA/settings.py
+++ CViA/CViA/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'CViA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'cvia',
+        'USER': 'cvia_user',
+        'PASSWORD': 'cviapass',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
