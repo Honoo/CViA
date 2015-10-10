7292b22bc1d7f764e23dd16b9faa46a882120413
Yos Riady <yosriady@gmail.com>
2015-10-10 13:40:36 +0800

    Add sample CVs

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import os
 from CVParser.cv_parser import CVParser
 from JobMatcher.job_matcher import JobMatcher
 
-resume = CVParser.parse_and_get_content(os.path.abspath('pdfs/sample2.pdf'))
+resume = CVParser.parse_and_get_content(os.path.abspath('pdfs/LinkedIn/PraveenDeorani.pdf'))
+
 print resume
+
+# Write resume.txt to file
+# f = open('resume.txt', 'w')
+# f.write(str(resume))
+# f.close()
+
 print JobMatcher.score(resume, JobMatcher.load_json(os.path.abspath('JobMatcher/query.json')))
