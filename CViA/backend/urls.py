2c2d7c1793d6c92d5efd1f3e1ee8d9f0dcd0cd5a
Su Gi Chandran <sollercoaster@hotmail.com>
2015-10-31 17:02:16 +0800

    Working UI

--- CViA/backend/urls.py
+++ CViA/backend/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^upload_file/$', views.upload_file, name='upload_file'),
+    url(r'^upload_successful/$', views.upload_successful, name='upload_successful'),
 ]
--- CViA/backend/views.py
+++ CViA/backend/views.py
@@ -1,22 +1,28 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 from .forms import UploadFileForm
+import os
 
 def index(request):
     return HttpResponse("Hello World")
 
+@csrf_exempt
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('fileuploaded.html')
+            return HttpResponseRedirect('../upload_successful')
     else:
         form = UploadFileForm()
     return render_to_response('upload_cv.html', {'form': form})
 
 def handle_uploaded_file(ufile):
-    with open("resume/{0}".format(ufile), "wb+") as target:
-        for chunk in f.chunks():
+    with open("{0}/resume/{1}".format(os.path.dirname(os.path.abspath(__file__)), ufile), "wb+") as target:
+        for chunk in ufile.chunks():
             target.write(chunk)
+
+def upload_successful(request):
+    return render_to_response("file_uploaded.html")
 
