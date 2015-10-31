3d6edc91eb629af6a07463fa2c1823916ec2a32f
Minqi Chen <cnchenminqi@gmail.com>
2015-10-31 16:30:47 +0800

    Add resume manager to create from pdf

--- CViA/backend/models.py
+++ CViA/backend/models.py
@@ -1,8 +1,26 @@
+import os
+
 from django.db import models
-# Create your models here.
+from CVParser.cv_parser import CVParser
+
+
+class ResumeManager(models.Manager):
+	def import_from_pdf(self, path):
+		if not os.path.isabs(path):
+			path = os.path.os.path.abspath(path)
+		resume_content = CVParser.parse_and_get_content(path)
+		resume = self.model()
+		for field, value in resume_content.iteritems():
+			if hasattr(resume, field):
+				setattr(resume, field, value)
+		return resume
+
+	def create_from_pdf(self, path):
+		resume = self.import_from_pdf(path)
+		resume.save()
+		return resume
 
 class Resume(models.Model):
-	# pid = models.PositiveIntegerField(primary_key=True, )
 	name = models.CharField(max_length=255)
 	email = models.CharField(max_length=255)
 	phone = models.CharField(max_length=255)
@@ -19,6 +37,9 @@ class Resume(models.Model):
 	projects = models.TextField()
 	summary = models.TextField()
 	objective = models.TextField()
+
+	objects = ResumeManager()
+
 
 class JobDescription(models.Model):
 	job_title = models.TextField()
