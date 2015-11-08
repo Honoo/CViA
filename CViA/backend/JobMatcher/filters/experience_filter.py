ca33b4eed3431bd44158d76d23c69e348b9d2323
Minqi Chen <cnchenminqi@gmail.com>
2015-11-08 16:21:48 +0800

    Validate experience scoring

--- CViA/backend/JobMatcher/filters/experience_filter.py
+++ CViA/backend/JobMatcher/filters/experience_filter.py
@@ -5,9 +5,13 @@ from filter import Filter
 class ExperienceFilter(Filter):
     @classmethod
     def run(cls, resume_attribute, query_attribute):
+        if query_attribute <= 0.001 or not isinstance(query_attribute, float):
+            print "Error"
+            return 0.0
         grp = re.findall('[1-2]{1}[0-9]{3}|(?<= - )current|present|now|(?<=-)current|present|now|(?<=to)current|present|now', resume_attribute, re.IGNORECASE)
         highest = 0
         lowest = datetime.datetime.now().year
+        print grp
         for g in grp:
             if g == 'present' or g == 'current' or g=='now':
                 highest = datetime.datetime.now().year
@@ -16,4 +20,5 @@ class ExperienceFilter(Filter):
                     lowest = int(g)
                 if int(g) > highest:
                     highest = int(g)
-        return float(highest-lowest)/query_attribute
+        score = float(highest-lowest)/query_attribute
+        return 1 if score < 0 else score
