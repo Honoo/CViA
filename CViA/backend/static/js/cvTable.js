1581883f5bd61d8fa34e2e4037187985c7095d68
Su Gi Chandran <sollercoaster@hotmail.com>
2015-11-08 21:16:54 +0800

    Added headers and templates

---
+++ CViA/backend/static/js/cvTable.js
@@ -0,0 +1,38 @@
+$(document).ready( function () {
+    renderTable("#cv_list");
+    $('tr').on('click','input[value="Edit"]', editDesc);
+});
+
+function renderTable(target) {
+    $(target).DataTable({
+        'ajax': { 
+            'url': '../get_job_descriptions',
+            'dataSrc': processData
+        },
+        'deferRender': true,
+        'columns': [
+            {'data': 'fields.name', 'title': 'Name'},
+            {'data': 'fields.email', 'title': 'Email'},
+            {'data': 'fields.phone', 'title': 'Phone'},
+            {'data': 'fields.skills', 'title': 'Skills'},
+            {'data': 'fields.experience', 'title': 'Experience'},
+            {'data': 'fields.education', 'title': 'Education'},
+            {'data': 'fields.languages', 'title': 'Languages'},
+            {'data': 'fields.edit', 'title': 'Edit'}
+        ]
+    });
+}
+
+function processData(json){
+    var len = json.length;
+    for(var i=0; i < len; i++){ 
+        json[i].DT_RowId = json[i].pk;
+        json[i].fields.edit = '<a href="'+json[i].pk+'/edit"><input type="button" value="Edit"></a>';
+    }
+    return json;
+}
+
+function editDesc(){
+    $('#descModalTitle').text($(this).parents('tr').children('td')[0].innerText);
+    $('#descModal').modal('show');
+}
