f2460b737b10ce6329225c52ca0d98caf5cc1a49
Yos Riady <yosriady@gmail.com>
2015-11-09 09:26:23 +0800

    Additional validations

--- JobMatcher/filters/experience_filter.py
+++ JobMatcher/filters/experience_filter.py
@@ -5,6 +5,9 @@ from filter import Filter
 class ExperienceFilter(Filter):
     @classmethod
     def run(cls, resume_attribute, query_attribute):
+        if not isinstance(query_attribute, float) or not isinstance(query_attribute, int):
+            return 0
+
         grp = re.findall('[1-2]{1}[0-9]{3}|(?<= - )current|present|now|(?<=-)current|present|now|(?<=to)current|present|now', resume_attribute, re.IGNORECASE)
         highest = 0
         lowest = datetime.datetime.now().year
